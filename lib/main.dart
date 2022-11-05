a043d7694791c5c3b27a0742dc401bdf3756b4b8
unknown <fahmiabdulaziz44@gmail.com>
2022-11-05 15:43:25 +0700

    inject provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todo_app_dengan_advanced_provider/pages/todos_page.dart';
+import 'package:todo_app_dengan_advanced_provider/providers/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +12,56 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        //todo 2
+        ChangeNotifierProvider<TodoFilter>(
+          create: (_) => TodoFilter(),
+        ),
+        //todo 3
+        ChangeNotifierProvider<TodoList>(
+          create: (_) => TodoList(),
+        ),
+        //todo 4
+        ChangeNotifierProvider<TodoSearch>(
+          create: (_) => TodoSearch(),
+        ),
+        //todo 5
+        ChangeNotifierProxyProvider<TodoList, ActiveTodoCount>(
+          create: (context) => ActiveTodoCount(),
+          update: (
+            BuildContext context,
+            TodoList todoList,
+            ActiveTodoCount? activeTodoCount,
+          ) =>
+              activeTodoCount!..update(todoList),
+        ),
+        //todo 6 (finish)
+        ChangeNotifierProxyProvider3<TodoFilter, TodoSearch, TodoList,
+            FilteredTodos>(
+          create: (context) => FilteredTodos(),
+          update: (
+            BuildContext context,
+            TodoFilter todoFilter,
+            TodoSearch todoSearch,
+            TodoList todoList,
+            FilteredTodos? filteredTodos,
+          ) =>
+              filteredTodos!
+                ..update(
+                  todoFilter: todoFilter,
+                  todoSearch: todoSearch,
+                  todoList: todoList,
+                ),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: TodosPage(),
       ),
-      home: TodosPage(),
     );
   }
 }
---
+++ lib/providers/provider.dart
@@ -0,0 +1,7 @@
+//todo 1 (next main.dart)
+
+export 'active_todo_count.dart';
+export 'filtered_todos.dart';
+export 'todo_filter.dart';
+export 'todo_list.dart';
+export 'todo_search.dart';
